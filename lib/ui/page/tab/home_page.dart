58a174efa26afd577e81a644ab85e4a183c8bce9
lb891014 <lb891014@126.com>
2020-03-01 00:30:23 +0800

    产生一些随机搜索词

--- lib/ui/page/tab/home_page.dart
+++ lib/ui/page/tab/home_page.dart
@@ -1,9 +1,12 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_music_app/generated/i18n.dart';
 import 'package:flutter_music_app/model/albums_model.dart';
 import 'package:flutter_music_app/model/for_you_model.dart';
 import 'package:flutter_music_app/model/song_model.dart';
 import 'package:flutter_music_app/provider/provider_widget.dart';
+import 'package:flutter_music_app/provider/view_state_widget.dart';
 import 'package:flutter_music_app/ui/widget/albums_carousel.dart';
 import 'package:flutter_music_app/anims/record_anim.dart';
 import 'package:flutter_music_app/ui/widget/for_you_carousel.dart';
@@ -21,6 +24,8 @@ class _HomePageState extends State<HomePage>
   Animation<double> animationRecord;
   final _inputController = TextEditingController();
   final _commonTween = new Tween<double>(begin: 0.0, end: 1.0);
+  static const albumValueList = ['酒吧', '怀旧', '女歌手', '经典', '热门'];
+  static const forYouValueList = ['华语', '流行', '轻音乐', '排行榜', '抖音'];
 
   @override
   bool get wantKeepAlive => true;
@@ -49,7 +54,11 @@ class _HomePageState extends State<HomePage>
 
   @override
   Widget build(BuildContext context) {
+    super.build(context);
     SongModel songModel = Provider.of(context);
+    Random r = new Random();
+    int _randomSongAlbum = r.nextInt(albumValueList.length);
+    int _randomSongForYou = r.nextInt(forYouValueList.length);
     if (songModel.isPlaying) {
       controllerRecord.forward();
     } else {
@@ -63,10 +72,17 @@ class _HomePageState extends State<HomePage>
               await alubumsModel.initData();
               await forYouModel.initData();
             },
-            model1: AlbumsModel(input: '老歌'),
-            model2: ForYouModel(input: '精选'),
+            model1: AlbumsModel(input: albumValueList[_randomSongAlbum]),
+            model2: ForYouModel(input: forYouValueList[_randomSongForYou]),
             autoDispose: false,
             builder: (context, alubumsModel, forYouModel, child) {
+              if (alubumsModel.busy) {
+                return ViewStateBusyWidget();
+              } else if (alubumsModel.error && alubumsModel.list.isEmpty) {
+                return ViewStateErrorWidget(
+                    error: alubumsModel.viewStateError,
+                    onPressed: alubumsModel.initData);
+              }
               return Column(children: <Widget>[
                 Row(
                   children: <Widget>[
